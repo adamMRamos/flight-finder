9451d0c873cf54d191f94b48fb1425862454dfde
Adam Ramos <amramos0828@gmail.com>
2020-01-13 13:06:13 -0500

    initial commit with gitignore

---
+++ src/com/amramos/flightfinder/Main.java
@@ -0,0 +1,11 @@
+package com.amramos.flightfinder;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("start");
+    }
+
+    public static boolean flightExists(String start, String end) {
+        return false;
+    }
+}
